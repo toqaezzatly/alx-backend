60e33b6874ce8c0c557a66ae9e69689ea62fa933
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-30 19:42:10 +0300

    lif

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""lifo caching"""
+"""LIFO caching system"""
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """first in first out"""
+    """LIFO caching system"""
     def __init__(self):
         super().__init__()
-        self.last_key = None
 
     def put(self, key, item):
-        """add item in the cache following LIFo replac. policy"""
+        """Add an item to the cache using LIFO replacement policy"""
         if key is None or item is None:
             return
 
-        if key in self.cache_data:
-            del self.cache_data[key]
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            if self.last_key is not None:
-                del self.cache_data[self.last_key]
-                print(f"DISCARD: {self.last_key}")
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
+                key not in self.cache_data):
+            # Get the last key added
+            last_key = list(self.cache_data.keys())[-1]
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
 
         self.cache_data[key] = item
-        self.last_key = key
 
     def get(self, key):
+        """Get an item by key"""
         return self.cache_data.get(key, None)
