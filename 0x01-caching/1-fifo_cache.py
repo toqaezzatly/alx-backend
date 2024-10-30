0ef319656f9ff0255e0b4d841af088f6ea2b317b
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-30 10:42:24 +0300

    FiFo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,18 +15,17 @@ class FIFOCache(BaseCaching):
         """Add an item in the cache"""
         if key is None or item is None:
             return
-        
+
         if key in self.cache_data:
             self.key_order.remove(key)
         self.cache_data[key] = item
         self.key_order.append(key)
-        
+
         if len(self.key_order) > BaseCaching.MAX_ITEMS:
             first_key = self.key_order.pop(0)
             del self.cache_data[first_key]
             print("DISCARD:", first_key)
-            
+
     def get(self, key):
         """Get an item by key"""
         return self.cache_data.get(key, None)
-    
