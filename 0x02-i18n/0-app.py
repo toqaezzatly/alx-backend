9d7d0b64576feecf6bc2feb6f48db2a3a61dca7a
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-05 15:59:26 +0200

    strictslashes and method for better ui and make it more secure

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     return render_template('0-index.html')
 
