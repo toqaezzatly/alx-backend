8b2f6be3080acffdf16a5a6b71ebeb0da98f8d38
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-05 18:21:24 +0200

    config

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel for i18n
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    Render the index page with locale
+    """
+    locale = get_locale()
+    return render_template('3-index.html', locale=locale)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
